4f26ee567626a618b1c2cee83288836abc964da3
NoothangiBharathi <noothangi.bharathi1999@gmail.com>
2024-06-15 12:53:07 +0530

    javascriptlearning

---
+++ index.js
@@ -0,0 +1,108 @@
+/*let day=4;
+
+switch(day){
+    case 1:
+        console.log("Today is Sunday");
+        break;
+    case 2:
+        console.log("Today is Monday");
+        break;
+    case 3:
+        console.log("Today is Tuesday");
+        break;
+    case 4:
+        console.log("Today is Wednesday");
+        break;
+    case 5:
+        console.log("Today is thrusday");
+        break;
+    case 6:
+        console.log("Today is Friday");
+        break;
+    case 7:
+        console.log("Today is Satureday");
+        break;
+    default:
+        console.log('${day} is not a day');     
+}*/
+
+
+//method chaining - calling one method after another method in one continuous line of code
+
+/*let username= window.prompt("Enter Your User Name:");
+
+//.......... No Method chaining.......
+
+let username=window.prompt("Enter Your Name:")
+username=username.trim();
+let letter=username.charAt(0);
+letter=letter.toUpperCase();
+
+let extrachars=username.slice(1);
+extrachars=extrachars.toLowerCase();
+username=letter + extrachars;
+
+console.log(username);
+
+
+//.......... method Chaining........*/
+
+
+/*const temp=200;
+if(temp>0){
+    document.write("The weather is GOOD")
+}
+else if(temp>30){
+   document.write("The weather is GOOD");
+}
+else{
+    console.log("The weather is BAD")
+}*/
+
+
+
+ /*const temp=25;
+
+ if(temp>0 && temp<=30){
+    document.write("The Weather is GOOD")
+ }
+ else{
+    document.write("The Weather is BAD")
+ }*/
+
+
+   /* const issunny=true;
+
+    if(!issunny){
+        document.write("It is cloudy");
+    }
+    else{
+        document.write("It is sunny");
+    }
+
+
+
+    const isbunny=false;
+
+    if(!issunny){
+        document.write("It is cloudy");
+    }
+    else{
+        document.write("It is bunny");
+    }*/
+
+
+
+    //strict inequality
+
+   /* let username="";
+
+
+    while(username === ""){
+       console.log("You don't enter your name")
+    }*/
+
+
+
+      /* let username=`deva`;
+       console.log(`your name is ${username}`); */
